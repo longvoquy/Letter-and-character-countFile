7932daecde6935c9f75d119620d4abb10f4742c1
longvoquyhoang <longvolongvo360@gmail.com>
2023-09-13 11:57:52 +0700

    check

--- src/Counter.java
+++ src/Counter.java
@@ -1,14 +1,10 @@
-package counter;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-/**
- *
- * @author THAYCACAC
- */
+
 public class Counter {
 
     private Map<Character, Integer> charCounter =
